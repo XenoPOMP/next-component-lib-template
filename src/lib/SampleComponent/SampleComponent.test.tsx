061603a5b5d53e7a00304b678d46151bc47e6b46
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-01-22 13:15:51 +0300

    replace test to dedicated folder

--- src/lib/SampleComponent/SampleComponent.test.tsx
+++ src/lib/SampleComponent/SampleComponent.test.tsx
@@ -1,6 +1,7 @@
 import { describe, expect, test } from 'vitest';
 import { render } from '@testing-library/react';
-import SampleComponent from '../src';
+
+import SampleComponent from './SampleComponent';
 
 describe('Sample Vitest test', () => {
   test('It works!', () => {
